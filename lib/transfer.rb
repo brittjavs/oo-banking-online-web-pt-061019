08657a51b60d67ccde4eba67f89388c25f60da60
Brittany Javalera <brittjavalera@gmail.com>
2019-07-13 00:22:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end
   
   def execute_transaction
-    if @status == valid? && "pending"
+    if @status == "pending" && valid?
       sender.balance -= amount
       receiver.balance += amount
       self.status = "complete"
